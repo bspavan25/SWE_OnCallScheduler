6603f65f1a0440c721fdb59aec2ea88433edb1b6
bspavan25 <bspavan25@gmail.com>
2022-11-02 18:40:29 -0400

    Commit 1.0- Django server and schema setup

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    employee_id = models.IntegerField()
+    name = models.CharField( max_length=200)
+    skills = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Availability(models.Model):
+    employee = models.ForeignKey(Employee, on_delete = models.CASCADE)
+    numberOfHours = models.IntegerField()
+
+    def __str__(self):
+        return self.employee.name + " " + self.numberOfHours
+
+class Skills(models.Model):
+    skill_id = models.IntegerField()
+    skill_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.skill_name
+
+    
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello world")
+    #emp1 
+    employees_list = Employee.objects.all()
+    for i in employees_list:
+        print(i.skills)
+    return render(request, 'home.html', {'emp1' : "sagar"})
+
+def add(request):
+    val1 = request.POST["num1"]
+    val2 = request.POST["num2"]
+    return render(request, "result.html", {'result': val1+val2})
