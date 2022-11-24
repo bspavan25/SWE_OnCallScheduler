27ac63b222cb341e822703ef13a6c7afb8e2fa30
bspavan25 <bspavan25@gmail.com>
2022-11-24 12:24:28 -0500

    More changes to UI of admin page

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
+admin.site.site_header = 'OnCall Scheduler'                   
+admin.site.index_title = 'Scheduler Administration'                 
+admin.site.site_title = 'OnCall Scheduler'
 
 urlpatterns= [
     path('', views.home, name="home")
